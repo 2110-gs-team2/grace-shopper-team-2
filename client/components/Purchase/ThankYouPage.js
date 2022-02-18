a2f79393125df8bfd8d4993b6da17710b06aec4e
Julie Kim <juliekim0918@gmail.com>
2022-02-17 22:10:29 -0500

    finalize thank you page

--- client/components/Purchase/ThankYouPage.js
+++ client/components/Purchase/ThankYouPage.js
@@ -14,9 +14,20 @@ const ThankYouPage = () => {
 
   return (
     <div className="w-screen h-screen bg-forest-green">
-      <div className="pt-36 pb-20 max-w-7xl m-auto h-full gap-5">
-        <div className=" p-10 bg-beige rounded-lg">
-          <div className="flex flex-col gap-3">Thank you</div>
+      <div className="pt-36 pb-20 max-w-7xl m-auto h-full">
+        <div className=" p-10 bg-beige rounded-lg ">
+          <div className="flex flex-col gap-2 max-w-xl m-auto">
+            <div className="text-3xl text-center">
+              Your order number {`#${params.orderId.split("-")[0]}`} is
+              confirmed!
+            </div>
+            <div className="text-xl text-center">
+              Thank you for your purchase. We're excited for you to start your
+              plant nurturing journey. You should receive an email confirmation
+              about your order! Please note that it will take about 5-7 business
+              days for your items to ship.
+            </div>
+          </div>
         </div>
       </div>
     </div>
