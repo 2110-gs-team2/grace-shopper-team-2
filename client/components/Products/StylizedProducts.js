138358ffbb3dc432e77dd1a33c6568a7b57cc87f
Julie Kim <juliekim0918@gmail.com>
2022-02-21 14:44:29 -0500

    Fix module typo

--- client/components/Products/StylizedProducts.js
+++ client/components/Products/StylizedProducts.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { ChevronDown } from "react-feather";
 import { useDispatch, useSelector } from "react-redux";
 import { Disclosure } from "@headlessui/react";
@@ -12,146 +12,75 @@ import startCase from "lodash/startCase";
 import ProductCard from "./ProductCard";
 import queryString from "query-string";
 
-const AllProducts = (props) => {
-  const [queryObj, setQueryObj] = useState({});
-  const [sortBy, setSortBy] = useState("");
+/* To-do
+- If products are already loaded in redux store, should we keep calling same thunk?
+- Filter products according to query strings (currently only works for one filter)
+- Create a clearFilters function
+- Modify url if user manually adds any filters
+- Add sort-by options and create function
+*/
+
+const StylizedProducts = (props) => {
+  let queryObj = {};
+  if (props.location.search.includes("?")) {
+    queryObj = queryString.parse(props.location.search);
+    // console.log(queryObj, "Query object"); // Remove after testing
+  }
 
   const dispatch = useDispatch();
   useEffect(() => {
+    // dispatch(getAllProducts());
     window.scrollTo({ top: 0, behavior: "smooth" });
-    setFilter(getQueryObjectFromUrl());
   }, []);
 
-  const changeSort = (evt) => {
-    setSortBy(evt.target.value);
-  };
+  //---This only works with one filter criteria---
+  // const currProducts = useSelector((state) => {
+  //   if (Object.keys(queryObj).length) {
+  //     for (const [key, value] of Object.entries(queryObj)) {
+  //       if (key === "isPetFriendly") {
+  //         const myBool = (value === "true");
+  //         return state.products.filter(
+  //           (product) => product[key] === myBool
+  //         );
+  //       } else {
+  //         return state.products.filter(
+  //           (product) => product[key] === value.toUpperCase()
+  //         );
+  //       }
+  //     }
+  //   }
+  //   return state.products;
+  // });
 
-  const clearFilters = (evt) => {
-    setFilter({});
-  };
-
-  const filterChange = (name, filterValue, event) => {
-    const qo = Object.assign({}, queryObj);
-    if (!qo[name]) {
-      qo[name] = filterValue.toLowerCase();
-    } else {
-      if (Array.isArray(queryObj[name])) {
-        if (event.target.checked) {
-          qo[name].push(filterValue.toLowerCase());
-        } else {
-          qo[name] = qo[name].filter((z) => z !== filterValue.toLowerCase());
-          if (!qo[name].length) delete qo[name];
-        }
-      } else {
-        if (event.target.checked) {
-          qo[name] = [qo[name]];
-          qo[name].push(filterValue.toLowerCase());
-        } else {
-          delete qo[name];
-        }
-      }
-    }
-    setFilter(qo);
-  };
-
-  const getQueryObjectFromUrl = () => {
-    return props.location.search.includes("?")
-      ? queryString.parse(props.location.search)
-      : {};
-  };
-
-  const getNewLocationFromQueryObj = (filter) => {
-    let newQueryString = queryString.stringify(filter);
-    newQueryString = newQueryString === "" ? "" : "?" + newQueryString;
-    return window.location.href.split("?")[0] + newQueryString;
-  };
-
-  const getSizeInt = (size) => {
-    switch (size.toLowerCase()) {
-      case "x-small":
-        return 0;
-      case "small":
-        return 1;
-      case "medium":
-        return 2;
-      case "large":
-        return 3;
-      default:
-        return -1;
-    }
-  };
-
-  const isInFilter = (name, filterValue) => {
-    return Object.entries(queryObj).some(([key, val]) => {
-      if (key === name) {
-        if (Array.isArray(val)) {
-          return val.includes(filterValue.toLowerCase());
-        } else {
-          return filterValue.toLowerCase() === String(val).toLowerCase();
-        }
-      }
-    });
-  };
-
-  const setBrowserQueryStringFromObj = (filter) => {
-    window.history.pushState(null, "", getNewLocationFromQueryObj(filter));
-  };
-
-  const setFilter = (filter) => {
-    setBrowserQueryStringFromObj(filter);
-    setQueryObj((prevState) => {
-      return { ...filter };
-    });
-  };
-
+  /*
+  ***In progress...probably need recursion***
+  Idea - Function takes in array of object.keys from the filters and array of objects to filter. Pop first key from obj.keys arr, filter objects array with it, if obj.keys array still not empty return a call to the same func. Passing in the obj.keys( minus the one just used ) and the filtered array. Else return the filtered array
+  */
   const currProducts = useSelector((state) => {
-    const products = state.products.filter((product) =>
-      Object.entries(queryObj).every(([key, val]) => {
-        if (val === "") return;
-        if (Array.isArray(val)) {
-          return val.some(
-            (v) =>
-              String(v).toUpperCase() === String(product[key]).toUpperCase()
-          );
+    if (Object.keys(queryObj).length) {
+      for (const [key, value] of Object.entries(queryObj)) {
+        if (key === "isPetFriendly") {
+          const myBool = value === "true";
+          return state.products.filter((product) => product[key] === myBool);
         } else {
-          return (
-            String(product[key]).toUpperCase() === String(val).toUpperCase()
+          return state.products.filter(
+            (product) => product[key] === value.toUpperCase()
           );
         }
-      })
-    );
-
-    if (sortBy === "name") {
-      return products.sort((a, b) => (a[sortBy] > b[sortBy] ? 1 : -1));
-    } else if (sortBy === "price") {
-      return products.sort((a, b) =>
-        Number(a[sortBy]) > Number(b[sortBy]) ? 1 : -1
-      );
-    } else if (sortBy === "size") {
-      return products.sort((a, b) =>
-        getSizeInt(a[sortBy]) > getSizeInt(b[sortBy]) ? 1 : -1
-      );
+      }
     }
-    return products;
+    return state.products;
   });
+  // console.log(currProducts, "CurrProducts"); // Remove after testing
 
   return (
     <div>
       <div className="min-h-[100vh] bg-beige">
         <div className="min-h-[120vh] pt-28 p-20 max-w-[90vw] m-auto">
           <div className="flex justify-between mb-5">
-            <div className="text-3xl">Filter ({currProducts.length})</div>
-            <select
-              defaultValue=""
-              onChange={(evt) => changeSort(evt)}
-              className="focus:outline-none block p-6 py-3  text-center rounded-full text-base font-bold bg-forest-green text-beige"
-            >
-              <option value="" disabled>
-                SORT BY
-              </option>
-              <option value="name">Name</option>
-              <option value="price">Price</option>
-              <option value="size">Size</option>
+            <div className="text-4xl">Filter</div>
+            <select className="focus:outline-none block p-6 py-3  text-center rounded-full text-base font-bold bg-forest-green text-beige uppercase">
+              <option value="">Sort by</option>
             </select>
           </div>
           <div className="grid grid-cols-4 m-auto h-full gap-5 my-5">
@@ -176,11 +105,14 @@ const AllProducts = (props) => {
                           <input
                             name={type}
                             type="checkbox"
-                            onChange={(event) =>
-                              filterChange("type", type, event)
-                            }
                             value={type}
-                            checked={isInFilter("type", type)}
+                            defaultChecked={
+                              Object.values(queryObj).includes(
+                                type.toLowerCase()
+                              )
+                                ? true
+                                : false
+                            }
                             className="h-8 w-8 border-2 rounded-lg border-forest-green  focus:text-forest-green focus:ring-forest-green focus:outline-none checked:hover:bg-forest-green checked:bg-forest-green bg-beige"
                           />
                           <label
@@ -215,20 +147,21 @@ const AllProducts = (props) => {
                           <input
                             name={size}
                             type="checkbox"
-                            onChange={(event) =>
-                              filterChange("size", size, event)
-                            }
                             value={size}
-                            checked={isInFilter("size", size)}
+                            defaultChecked={
+                              Object.values(queryObj).includes(
+                                size.toLowerCase()
+                              )
+                                ? true
+                                : false
+                            }
                             className="h-8 w-8 border-2 rounded-lg border-forest-green  focus:text-forest-green focus:ring-forest-green focus:outline-none checked:hover:bg-forest-green checked:bg-forest-green bg-beige"
                           />
                           <label
                             htmlFor={size}
                             className="ml-3 min-w-0 flex-1 text-lg"
                           >
-                            {size[0] === "X"
-                              ? size.slice(0, 3) + size.slice(-4).toLowerCase()
-                              : startCase(size.toLowerCase())}
+                            {startCase(size.toLowerCase())}
                           </label>
                         </div>
                       ))}
@@ -256,11 +189,14 @@ const AllProducts = (props) => {
                           <input
                             name={light}
                             type="checkbox"
-                            onChange={(event) =>
-                              filterChange("light", light, event)
-                            }
                             value={light}
-                            checked={isInFilter("light", light)}
+                            defaultChecked={
+                              Object.values(queryObj).includes(
+                                light.toLowerCase()
+                              )
+                                ? true
+                                : false
+                            }
                             className="h-8 w-8 border-2 rounded-lg border-forest-green  focus:text-forest-green focus:ring-forest-green focus:outline-none checked:hover:bg-forest-green checked:bg-forest-green bg-beige"
                           />
                           <label
@@ -296,10 +232,13 @@ const AllProducts = (props) => {
                             name={difficulty}
                             type="checkbox"
                             value={difficulty}
-                            onChange={(event) =>
-                              filterChange("difficulty", difficulty, event)
+                            defaultChecked={
+                              Object.values(queryObj).includes(
+                                difficulty.toLowerCase()
+                              )
+                                ? true
+                                : false
                             }
-                            checked={isInFilter("difficulty", difficulty)}
                             className="h-8 w-8 border-2 rounded-lg border-forest-green  focus:text-forest-green focus:ring-forest-green focus:outline-none checked:hover:bg-forest-green checked:bg-forest-green bg-beige"
                           />
                           <label
@@ -328,17 +267,14 @@ const AllProducts = (props) => {
                     <Disclosure.Panel className="px-4 pt-4 pb-2 ">
                       <div className="flex items-center form-check mb-3">
                         <input
-                          name="isPetFriendly"
+                          name="petFriendly"
                           type="checkbox"
                           value={false}
-                          onChange={(event) =>
-                            filterChange(
-                              "isPetFriendly",
-                              String(event.target.checked),
-                              event
-                            )
+                          defaultChecked={
+                            Object.keys(queryObj).includes("isPetFriendly")
+                              ? true
+                              : false
                           }
-                          checked={isInFilter("isPetFriendly", "true")}
                           className="h-8 w-8 border-2 rounded-lg border-forest-green  focus:text-forest-green focus:ring-forest-green focus:outline-none checked:hover:bg-forest-green checked:bg-forest-green bg-beige"
                         />
                         <label
@@ -352,10 +288,7 @@ const AllProducts = (props) => {
                   </>
                 )}
               </Disclosure>
-              <button
-                onClick={(evt) => clearFilters(evt)}
-                className="mt-5 uppercase p-6 py-3 w-full border-2 border-forest-green rounded-full text-base font-bold bg-beige text-forest-green "
-              >
+              <button className="mt-5 uppercase p-6 py-3 w-full border-2 border-forest-green rounded-full text-base font-bold bg-beige text-forest-green ">
                 Clear filters
               </button>
             </div>
@@ -375,4 +308,4 @@ const AllProducts = (props) => {
   );
 };
 
-export default AllProducts;
+export default StylizedProducts;
