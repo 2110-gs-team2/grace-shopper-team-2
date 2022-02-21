30165b60ede3506424b46984bed20f1af92c187f
Julie Kim <juliekim0918@gmail.com>
2022-02-21 14:49:05 -0500

    remove axios in db/models/User.js

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -3,7 +3,6 @@ const { STRING, ENUM, UUID, UUIDV4 } = Sequelize;
 const db = require("../db");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const axios = require("axios");
 
 const SALT_ROUNDS = 5;
 
